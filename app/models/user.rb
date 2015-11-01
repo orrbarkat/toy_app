57e7530c89e857a1464f08efcf25930e53de9fb5
orrbarkat <barkat.orr@gmail.com>
2015-11-01 11:35:00 +0200

    web trial

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
 	has_many :microposts
 	validates :name, presence: true
   	validates :email, presence: true
+  	
 end
---
+++ db/migrate/20150723081802_add_level_to_user.rb
@@ -0,0 +1,6 @@
+class AddLevelToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :level, :integer
+    
+  end
+end
